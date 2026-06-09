09d9dc7f73508b4aedaeab28f3c273f93b1ad7a8
Erich Miranda Coelho Benz <erich.benz@vixteam.com.br>
2026-06-09 18:46:42 -0300

    Ajuste config db

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
-import "dotenv/config"
+import "dotenv/config";
 
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL = process.env.MONGO_URL;
 
-async function connectDb () {
-    try {
-        await mongoose.connect(MONGO_URL)
-        console.log("✅Banco de dados conectado")
-    } catch (error) {
-        console.log("❌Erro ao conectar com o banco de dados", error)
-    }
+async function connectDb() {
+  try {
+    console.log("MONGO_URL existe?", !!MONGO_URL);
+
+    await mongoose.connect(MONGO_URL);
+
+    console.log("✅ Banco de dados conectado");
+  } catch (error) {
+    console.log("❌ Erro ao conectar");
+    console.log(error);
+  }
 }
 
 export default connectDb;
