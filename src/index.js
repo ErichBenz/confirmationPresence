ce740434a525ffb54ce630039b38372c281a616f
Erich Miranda Coelho Benz <erich.benz@vixteam.com.br>
2026-06-09 18:30:02 -0300

    Ajustando o index

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-import express from "express"
-import "dotenv/config"
-import connectDb from "./config/db.js"
-import router from "./routes/routes.js"
-import cors from "cors"
+import express from "express";
+import "dotenv/config";
+import connectDb from "./config/db.js";
+import router from "./routes/routes.js";
+import cors from "cors";
 
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors({
-    origin: "https://confirmationinvite.netlify.app/"
-}))
-app.use(express.json())
-app.use(router)
+
+app.use(
+  cors({
+    origin: "https://confirmationinvite.netlify.app",
+  })
+);
+
+app.use(express.json());
+app.use(router);
 
 const startServer = async () => {
-    await connectDb();
-    app.listen( PORT, () => console.log(`✅Servidor rodando na porta ${PORT}`))
-}
+  await connectDb();
+
+  app.listen(PORT, () => {
+    console.log(`✅ Servidor rodando na porta ${PORT}`);
+  });
+};
 
 startServer();
