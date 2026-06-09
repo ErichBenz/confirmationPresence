e20c9a087e52dd7a6e3c7f16175abf3d0d393533
Erich Miranda Coelho Benz <erich.benz@vixteam.com.br>
2026-06-09 18:49:39 -0300

    Ajustado

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,15 @@
 import mongoose from "mongoose";
-import "dotenv/config";
+import "dotenv/config"
 
-const MONGO_URL = process.env.MONGO_URL;
+const MONGO_URL = process.env.MONGO_URL
 
-async function connectDb() {
-  try {
-    console.log("MONGO_URL existe?", !!MONGO_URL);
-
-    await mongoose.connect(MONGO_URL);
-
-    console.log("✅ Banco de dados conectado");
-  } catch (error) {
-    console.log("❌ Erro ao conectar");
-    console.log(error);
-  }
+async function connectDb () {
+    try {
+        await mongoose.connect(MONGO_URL)
+        console.log("✅Banco de dados conectado")
+    } catch (error) {
+        console.log("❌Erro ao conectar com o banco de dados", error)
+    }
 }
 
 export default connectDb;
